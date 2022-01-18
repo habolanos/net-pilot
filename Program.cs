d5fada42495d91d433082f41e1eb1c074fecfeb9
Harold Adrian <haroldb@mill5.com>
2022-01-18 09:42:05 -0500

    Scope: add stage publish and add lines to history in at program  Changes to be committed: 	modified:   .gitlab-ci.yml 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace dotnetcore
         {
 	    Console.WriteLine("+------------------------------------------+");
 	    Console.WriteLine("| C# Program by Harold Adrian Bolaños R    |");
-            Console.WriteLine("| This is a .Net Pilot for rollout CICD    |");
-            Console.WriteLine("| Program made in linux ubuntu             |");
+        Console.WriteLine("| This is a .Net Pilot for rollout CI/CD   |");
+        Console.WriteLine("| Program made in linux ubuntu             |");
 	    Console.WriteLine("+------------------------------------------+");
+		Console.WriteLine("|                 History                  |");
+		Console.WriteLine("| 20220118 < tag v0.0.1                    |");
+		Console.WriteLine("+------------------------------------------+");
         }
     }
 }
