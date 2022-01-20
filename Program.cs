68021cd6aea1bcae322d4914ba64a800419f574b
HAROLD ADRIAN [mill5] <haroldb@mill5.com>
2022-01-20 08:27:33 -0500

    scope: minor references to space on sourcecode

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-	    Console.WriteLine("+------------------------------------------+");
-	    Console.WriteLine("| C# Program by Harold Adrian Bolaños R    |");
-            Console.WriteLine("| This is a .Net Pilot for rollout CI/CD   |");
-            Console.WriteLine("| Program made in linux ubuntu             |");
-	    Console.WriteLine("+------------------------------------------+");
-	    Console.WriteLine("|                 History                  |");
-	    Console.WriteLine("| 20220118 < tag v0.0.1                    |");
-	    Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("| C# Program by Harold Adrian Bolaños R    |");
+			Console.WriteLine("| This is a .Net Pilot for rollout CI/CD   |");
+			Console.WriteLine("| Program made in linux ubuntu             |");
+			Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("|                 History                  |");
+			Console.WriteLine("| 20220118 < tag v0.0.1                    |");
+			Console.WriteLine("+------------------------------------------+");
         }
     }
 }
